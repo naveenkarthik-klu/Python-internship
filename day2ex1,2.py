496a44de3982229163371df7389778cab16a0ce1
Naveen Karthik <61445694+naveenkarthik-klu@users.noreply.github.com>
2020-11-24 14:13:08 +0530

    Add files via upload

---
+++ day2ex1,2.py
@@ -0,0 +1,9 @@
+a = 20
+b = 30
+c = 50
+print(a/10)
+print(b*50)
+print(c+50)
+s = "abcde"
+
+print(s.replace("c", "G"))
---
+++ day2ex3.py
@@ -0,0 +1,8 @@
+a = 5
+b = 5.0
+print("before conversion")
+print("a = ", a)
+print("b = ", b)
+print("after conversion")
+print("a = ", float(a))
+print("b = ", int(b))
